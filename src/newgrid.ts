7bee604360780ac4edbf32b1498f2da0bcfd5545
Gijs van Tulder <gijs@vantulder.net>
2023-05-18 19:07:46 +0200

    Add overlap to fix edge artifacts.

--- src/newgrid.ts
+++ src/newgrid.ts
@@ -1,8 +1,12 @@
 
 
-const OFFSET = 50;
+const OFFSET = 0;
 const SCALE = 100;
+const DEBUG_OVERLAP = false;
+const O = (DEBUG_OVERLAP ? 0.1 : 0.01);
 const COLORS = ['black', 'red', 'blue', 'grey', 'green', 'brown', 'orange', 'purple', 'pink'];
+
+const SELECT_TRIANGLE = 2;
 
 
 type Coord = [x : number, y : number];
@@ -13,7 +17,8 @@ abstract class Triangle {
     y : number;
     color : string;
 
-    points : Coord[];
+    points : [Coord, Coord, Coord];
+    polyPoints : Coord[];
     left : number;
     top : number;
 
@@ -40,6 +45,7 @@ abstract class Triangle {
         div.style.position = 'absolute';
         div.style.left = `${this.left * SCALE + OFFSET}px`;
         div.style.top = `${this.top * SCALE + OFFSET}px`;
+        div.style.zIndex = `${this.x * 1000 + this.y}`;
 
         const svg = this.generateSvg();
         div.appendChild(svg);
@@ -47,21 +53,34 @@ abstract class Triangle {
 
     generateSvg() {
         const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-        svg.setAttribute('width', `${SCALE}`);
-        svg.setAttribute('height', `${SCALE}`);
+        svg.setAttribute('width', `${SCALE + 10}`);
+        svg.setAttribute('height', `${SCALE + 10}`);
 
         const group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
         svg.appendChild(group);
 
-        const coordinates = this.points.map((p) => `${p[0] * SCALE},${p[1] * SCALE}`);
+        const pointsString = [...this.points, this.points[0]].map((p) => `${p[0] * SCALE},${p[1] * SCALE}`);
+        if (!this.polyPoints) this.polyPoints = [...this.points, this.points[0]];
+        const polyString = this.polyPoints.map((p) => `${p[0] * SCALE},${p[1] * SCALE}`);
 
         const el = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
-        el.setAttribute('points', coordinates.join(' '));
-        el.setAttribute('opacity', '0.5');
+        el.setAttribute('points', polyString.join(' '));
+        if (DEBUG_OVERLAP) {
+            el.setAttribute('opacity', '0.6');
+        }
         el.setAttribute('fill', this.color);
-        el.setAttribute('stroke', 'white');
-        el.setAttribute('stroke-width', '2px');
+        // el.setAttribute('stroke', 'white');
+        // el.setAttribute('stroke-width', '2px');
         group.append(el);
+
+        if (DEBUG_OVERLAP) {
+            const outline = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
+            outline.setAttribute('points', pointsString.join(' '));
+            outline.setAttribute('fill', 'transparent');
+            outline.setAttribute('stroke', 'yellow');
+            outline.setAttribute('stroke-width', '1px');
+            group.append(outline);
+        }
 
         const center = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
         center.setAttribute('cx', `${this.center[0] * SCALE}`);
@@ -110,14 +129,17 @@ abstract class Triangle {
 
 class HexGridTriangle extends Triangle {
     calc() {
+        const height = Math.sqrt(3) / 2;
         // equilateral triangle in a hexagonal grid
         if (this.x % 2 == this.y % 2) {
             // triangle pointing down
-            this.points = [[0, 0], [0.5, Math.sqrt(3) / 2], [1, 0], [0, 0]];
+            this.points = [[0, 0], [1, 0], [0.5, height]];
+            this.polyPoints = [[0, 0], [1 + O, 0], [0.5, height + O], [0.5, height], [0, 0]];
             this.neighborOffsets = [[0, -1], [1, 0], [-1, 0]];
         } else {
             // triangle pointing up
-            this.points = [[0.5, 0], [0, Math.sqrt(3) / 2], [1, Math.sqrt(3) / 2], [0.5, 0]];
+            this.points = [[0.5, 0], [1, height], [0, height]];
+            this.polyPoints = [[0.5, 0], [0.5 + O, 0], [1 + O, height + O], [0, height + O], [0, height], [0.5, 0]];
             this.neighborOffsets = [[-1, 0], [1, 0], [0, 1]];
         }
 
@@ -133,21 +155,25 @@ class SquareGridTriangle extends Triangle {
         this.top = Math.floor(this.y / 4);
         if (this.y % 4 == 0) {
             // top triangle pointing down
-            this.points = [[0, 0], [0.5, 0.5], [1, 0], [0, 0]];
+            this.points = [[0, 0], [1, 0], [0.5, 0.5]];
+            this.polyPoints = [[0, 0], [1 + O, 0], [0.5 + O, 0.5 + O], [0, 0]];
             this.neighborOffsets = [[0, -1], [0, 1], [0, 2]];
         } else if (this.y % 4 == 1) {
             // left triangle pointing right
-            this.points = [[0, 0], [0.5, 0.5], [0, 1], [0, 0]];
+            this.points = [[0, 0], [0.5, 0.5], [0, 1]];
+            this.polyPoints = [[0, 0], [0.5 + O, 0.5 + O], [0, 1 + O], [0, 0]];
             this.neighborOffsets = [[-1, 1], [0, -1], [0, 2]];
         } else if (this.y % 4 == 2) {
             // right triangle pointing left
             this.left += 0.5;
-            this.points = [[0.5, 0], [0, 0.5], [0.5, 1], [0.5, 0]];
+            this.points = [[0, 0.5], [0.5, 0], [0.5, 1]];
+            this.polyPoints = [[0, 0.5], [0.5, 0], [0.5 + O, 0], [0.5 + O, 1 + O], [0.5, 1], [0, 0.5]];
             this.neighborOffsets = [[1, -1], [0, -2], [0, 1]];
         } else if (this.y % 4 == 3) {
             // bottom triangle pointing up
             this.top += 0.5;
-            this.points = [[0, 0.5], [0.5, 0], [1, 0.5], [0, 0.5]];
+            this.points = [[0, 0.5], [0.5, 0], [1, 0.5]];
+            this.polyPoints = [[0, 0.5], [0.5, 0], [1, 0.5], [1 + O, 0.5 + O], [0, 0.5 + O], [0, 0.5]];
             this.neighborOffsets = [[0, 1], [0, -1], [0, -2]];
         }
     }
@@ -157,39 +183,55 @@ class EquilateralGridTriangle extends Triangle {
     calc() {
         // triangle in a grid of equilateral triangles
         const height = Math.sqrt(3) / 2;
+        const h = height / 3;
         const odd = this.y % 12 < 6;
         this.left = this.x + (odd ? 0 : 0.5);
         this.top = height * Math.floor(this.y / 6);
         if (this.y % 6 == 0) {
             // top triangle pointing down
-            this.points = [[0, 0], [0.5, height / 3], [1, 0], [0, 0]];
+            this.points = [[0, 0], [1, 0], [0.5, h]];
+            this.polyPoints = [[0, 0], [1 + O, 0], [0.5, h + O], [0.5, h], [0, 0]];
             this.neighborOffsets = [[0, 1], [0, 2], [(odd ? -1 : 0), -1]];
         } else if (this.y % 6 == 1) {
             // left triangle pointing up-right
-            this.points = [[0, 0], [0.5, height / 3], [0.5, height], [0, 0]];
+            this.points = [[0, 0], [0.5, h], [0.5, height]];
+            this.polyPoints = [[0, 0], [0.5, h], [0.5 + O, h], [0.5 + O, height + O], [0.5, height], [0, 0]];
             this.neighborOffsets = [[0, -1], [-1, 3], [0, 1]];
         } else if (this.y % 6 == 2) {
             // right triangle pointing up-left
             this.left += 0.5;
-            this.points = [[0.5, 0], [0, height / 3], [0, height], [0.5, 0]];
+            this.points = [[0, h], [0.5, 0], [0, height]];
+            this.polyPoints = [[0, h], [0.5, 0], [0.5 + O, 0], [0, height + O], [0, height], [0, h]];
             this.neighborOffsets = [[0, -2], [0, -1], [0, 1]];
         } else if (this.y % 6 == 3) {
             // left triangle pointing bottom-right
             this.left += 0.5;
-            this.points = [[0.5, 0], [0.5, 2 * height / 3], [0, height], [0.5, 0]];
+            this.points = [[0, height], [0.5, 0], [0.5, 2 * h]];
+            this.polyPoints = [[0, height], [0.5, 0], [0.5 + O, 0], [0.5 + O, 2 * h + O], [0, height + O], [0, height]];
             this.neighborOffsets = [[0, -1], [0, 1], [0, 2]];
         } else if (this.y % 6 == 4) {
             // right triangle pointing bottom-left
             this.left += 1;
-            this.points = [[0, 0], [0.5, height], [0, 2 * height / 3], [0, 0]];
+            this.points = [[0, 0], [0.5, height], [0, 2 * h]];
+            this.polyPoints = [[0, 0], [O, 0], [0.5 + O, height + O], [0, 2 * h + O], [0, 0]];
             this.neighborOffsets = [[0, -1], [0, 1], [1, -3]];
         } else if (this.y % 6 == 5) {
             // bottom triangle pointing up
             this.left += 0.5;
-            this.top += 2 * height / 3;
-            this.points = [[0, height / 3], [0.5, 0], [1, height / 3], [0, height / 3]];
+            this.top += 2 * h;
+            this.points = [[0, h], [0.5, 0], [1, h]];
+            this.polyPoints = [[0, h], [0.5, 0], [1, h], [1 + O, h + O], [0, h + O], [0, h]];
             this.neighborOffsets = [[0, -2], [0, -1], [(odd ? 0 : 1), 1]];
         }
+    }
+}
+
+
+class Tile {
+    triangles : Triangle[];
+
+    constructor(triangles : Triangle[]) {
+        this.triangles = triangles;
     }
 }
 
@@ -205,10 +247,6 @@ class Connector {
     build() {
         const div = document.createElement('div');
         this.element = div;
-
-        div.style.position = 'absolute';
-        div.style.left = `${OFFSET}px`;
-        div.style.top = `${OFFSET}px`;
 
         const svg = this.generateSvg();
         this.element.appendChild(svg);
@@ -226,8 +264,8 @@ class Connector {
         return svg;
     }
 
-    connect(grid : NewGrid, triangle : Triangle) {
-        for (const neighbor of grid.getNeighbors(triangle)) {
+    connect(triangle : Triangle, neighbors : Triangle[]) {
+        for (const neighbor of neighbors) {
             this.drawLine(triangle, neighbor);
         }
     }
@@ -238,20 +276,26 @@ class Connector {
         line.setAttribute('y1', `${(a.top + a.center[1]) * SCALE}`);
         line.setAttribute('x2', `${(b.left + b.center[0]) * SCALE}`);
         line.setAttribute('y2', `${(b.top + b.center[1]) * SCALE}`);
+        line.setAttribute('opacity', '0.5');
         line.setAttribute('stroke', 'red');
-        line.setAttribute('stroke-width', '8');
+        line.setAttribute('stroke-width', '2');
         this.svgGroup.appendChild(line);
     }
 }
 
 
-export class NewGrid {
-    triangleType = [HexGridTriangle, SquareGridTriangle, EquilateralGridTriangle][2];
+export class GridDisplay {
+    grid : NewGrid;
+    element : HTMLDivElement;
+    gridElement : HTMLDivElement;
 
-    grid : Triangle[][];
-    div : HTMLDivElement;
+    constructor(grid : NewGrid) {
+        this.grid = grid;
 
-    constructor() {
+        this.build();
+    }
+
+    build() {
         const div = document.createElement('div');
         div.style.position = 'fixed';
         div.style.top = '0px';
@@ -259,36 +303,66 @@ export class NewGrid {
         div.style.width = '100%';
         div.style.height = '100%';
         div.style.background = '#fff';
-        div.style.zIndex = '10000';
-        document.body.appendChild(div);
-        this.div = div;
+        div.style.zIndex = '1000';
+        this.element = div;
 
-        this.drawTriangles();
+        const gridElement = document.createElement('div');
+        gridElement.style.position = 'absolute';
+        gridElement.style.top = '10px';
+        gridElement.style.left = '10px';
+        gridElement.style.zIndex = '100';
+        this.gridElement = gridElement;
+        this.element.appendChild(gridElement);
     }
 
     drawTriangles() {
-        const triangles : Triangle[] = [];
-        const grid : Triangle[][] = [];
-        this.grid = grid;
+        for (const triangle of this.grid.triangles) {
+            this.gridElement.appendChild(triangle.element);
+        }
 
+        const conn = new Connector();
+        this.element.appendChild(conn.element);
+        conn.element.style.position = 'absolute';
+        conn.element.style.top = '10px';
+        conn.element.style.left = '10px';
+        conn.element.style.zIndex = '200';
+
+        for (const triangle of this.grid.triangles) {
+            conn.connect(triangle, this.grid.getNeighbors(triangle));
+        }
+    }
+}
+
+
+export class NewGrid {
+    triangleType = [HexGridTriangle, SquareGridTriangle, EquilateralGridTriangle][SELECT_TRIANGLE];
+
+    grid : Triangle[][];
+    triangles : Triangle[];
+    div : HTMLDivElement;
+
+    constructor() {
+        this.grid = [];
+        this.triangles = [];
+
+        this.createTriangles();
+
+        const display = new GridDisplay(this);
+        display.drawTriangles();
+        document.body.appendChild(display.element);
+    }
+
+    createTriangles() {
         let i = 0;
         for (let row=0; row<24; row++) {
             for (let col=0; col<12; col++) {
                 const triangle = new this.triangleType(col, row, COLORS[i % COLORS.length]);
-                this.div.appendChild(triangle.element);
                 i++;
 
-                if (!grid[col]) grid[col] = [];
-                grid[col][row] = triangle;
-                triangles.push(triangle);
+                if (!this.grid[col]) this.grid[col] = [];
+                this.grid[col][row] = triangle;
+                this.triangles.push(triangle);
             }
-        }
-
-        const conn = new Connector();
-        this.div.appendChild(conn.element);
-
-        for (const triangle of triangles) {
-            conn.connect(this, triangle);
         }
     }
 

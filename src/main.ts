f5f1b988b174b8432cbf93730a7c604170c48cd8
Gijs van Tulder <gijs@vantulder.net>
2023-05-23 15:02:13 +0200

    Another example.

--- src/main.ts
+++ src/main.ts
@@ -49,6 +49,19 @@ export function runEditorDebug() {
             [ [0, -1], [0, 0], [1, 0], [1, 1], [0, 1], [0, 2], [-1, 1], [-1, 0] ],
             // [ [3, 0], [3, 1], [4, 1], [4, 2], [3, 2], [3, 3], [2, 2], [2, 1] ],
             [ [3, 0], [3, 1], [4, 1], [4, 2], [3, 2], [3, 3], [2, 2], [1, 2], [1,3], [2, 1] ],
+            [ [2,0], [2, -1], [3, -1], [4, -1], [3, -2], [4, 0] ],
+        ];
+        gridType = GridTypes['hex'];
+        break;
+
+    case '#rhombus-small':
+        // small, two-triangle rhombus
+        // clockwise
+        triangleOffsets = [
+            [ [1, 0], [2, 0] ],
+            [ [3, 0], [3, 1] ],
+            [ [1, 1], [2, 1] ],
+            [ [4, 1], [4, 2] ],
         ];
         gridType = GridTypes['hex'];
         break;

ef57983beb2db49dc7ad1fe644feb8f2da45689f
Gijs van Tulder <gijs@vantulder.net>
2023-07-04 00:44:58 +0200

    Build fonts on cloudflare.

--- build-cloudflare.sh
+++ build-cloudflare.sh
@@ -2,9 +2,11 @@
 
 rm -rf public
 mkdir -p public
+mkdir -p public/fonts
 
 cp style.css public/
 cp icon.png public/
+cp fonts/* public/fonts/
 
 timestamp=$(date +%s)
 sed -E 's/(style.css|main.js|icon.png)/\1?'$timestamp'/' < index.html > public/index.html

7b8a538b2fb56f6e2f82a6327beb84c22bc621d7
Gijs van Tulder <gijs@vantulder.net>
2023-07-07 18:05:48 +0200

    Add version to sw.js.

--- build-cloudflare.sh
+++ build-cloudflare.sh
@@ -5,5 +5,7 @@ rm -rf public
 npm install --include=dev
 rollup -c rollup-production.config.js
 
-sed --in-place "s/{version}/$(git rev-parse --short HEAD)/" public/dist/main.js
+version="$(git rev-parse --short HEAD)"
+sed --in-place "s/{version}/${version}/" public/dist/main.js
+echo "const version='${version}';" >> public/sw.js
 

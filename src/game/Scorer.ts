783417aa71dcbe9d0e5514f85700ace21b77beca
Gijs van Tulder <gijs@vantulder.net>
2025-03-01 13:49:04 +0100

    Add new Scorer.

---
+++ src/game/Scorer.ts
@@ -0,0 +1,88 @@
+import { Tile, TileColor, TileSegment } from "../geom/Tile";
+
+export type ScoredRegion = {
+    origin: TileSegment;
+    color: TileColor;
+    tiles: Set<Tile>;
+    segments: Set<TileSegment>;
+    edges: { from: TileSegment; to: TileSegment }[];
+    finished: boolean;
+    points: number;
+};
+
+export class Scorer {
+    /**
+     * Computes the score after placing the given tile.
+     *
+     * @param tile the last tile played
+     * @param includeIncomplete return incomplete regions?
+     * @returns the regions found from the tile
+     */
+    static computeScores(
+        tile: Tile,
+        includeIncomplete?: boolean,
+    ): ScoredRegion[] {
+        const visited = new Set<TileSegment>();
+
+        const shapes: ScoredRegion[] = [];
+
+        // visit each segment of the shape in turn
+        for (const origin of tile.segments) {
+            if (!visited.has(origin)) {
+                const color = origin.color;
+                const tilesInShape = new Set<Tile>();
+                const segmentsInShape = new Set<TileSegment>();
+                const edgesInShape: ScoredRegion["edges"] = [];
+
+                const shape: ScoredRegion = {
+                    origin: origin,
+                    color: origin.color,
+                    tiles: tilesInShape,
+                    segments: segmentsInShape,
+                    edges: edgesInShape,
+                    finished: true,
+                    points: 0,
+                };
+                shapes.push(shape);
+
+                // start at origin
+                const queue: TileSegment[] = [origin];
+                visited.add(origin);
+
+                while (queue.length > 0) {
+                    // process segment from queue
+                    const segment = queue.pop();
+                    tilesInShape.add(segment.tile);
+                    segmentsInShape.add(segment);
+
+                    // add neighbors
+                    for (const neighbor of segment.getNeighbors(true)) {
+                        if (!neighbor || !neighbor.color) {
+                            shape.finished = false;
+                        } else if (neighbor.color === color) {
+                            if (!visited.has(neighbor)) {
+                                queue.push(neighbor);
+                                visited.add(neighbor);
+                            }
+                            edgesInShape.push({ from: segment, to: neighbor });
+                        }
+                    }
+                }
+            }
+        }
+
+        for (const shape of shapes) {
+            if (shape.finished) {
+                // double points for shapes with four tiles or more
+                shape.points =
+                    (shape.tiles.size > 3 ? 2 : 1) * shape.tiles.size;
+            }
+        }
+
+        if (includeIncomplete) {
+            return shapes;
+        } else {
+            return shapes.filter((s) => s.finished);
+        }
+    }
+}

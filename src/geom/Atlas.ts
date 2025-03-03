0c6074ac0d31ccf8454e1c779d7357e5d7d6af9b
Gijs van Tulder <gijs@vantulder.net>
2025-03-03 23:35:28 +0100

    Add rhombus and hexagon games.

--- src/geom/Atlas.ts
+++ src/geom/Atlas.ts
@@ -246,6 +246,20 @@ export const TrianglesAtlas = Atlas.fromDefinition({
     vertices: [{ name: "triangle", vertex: "T0-T0-T0-T0-T0-T0" }],
 });
 
+export const RhombusAtlas = Atlas.fromDefinition({
+    name: "Rhombus-60-120",
+    shapes: {
+        L: { name: "rhombus", angles: [60, 120, 60, 120] },
+    },
+    vertices: [
+        { name: "a", vertex: "L0-L0-L0-L0-L0-L0" },
+        { name: "b", vertex: "L1-L0-L0-L0-L0" },
+        { name: "c", vertex: "L1-L1-L0-L0" },
+        { name: "d", vertex: "L1-L0-L1-L0" },
+        { name: "e", vertex: "L1-L1-L1" },
+    ],
+});
+
 export const HexagonsAtlas = Atlas.fromDefinition({
     name: "Hexagon",
     shapes: {
--- src/saveGames.ts
+++ src/saveGames.ts
@@ -1,6 +1,11 @@
 import { GameSettings } from "./game/Game";
 import { TileGenerators } from "./game/TileGenerator";
-import { SquaresAtlas, TrianglesAtlas } from "./geom/Atlas";
+import {
+    HexagonsAtlas,
+    RhombusAtlas,
+    SquaresAtlas,
+    TrianglesAtlas,
+} from "./geom/Atlas";
 
 const COLORS = [
     "red",
@@ -43,13 +48,59 @@ lookup.set("square5", {
 
 lookup.set("triangle", {
     atlas: TrianglesAtlas,
-    initialTile: ["red", "black", "blue"],
+    initialTile: ["#00c0ef", "#dd4b39", "#f39c12"],
     tilesShownOnStack: 3,
     tileGenerator: [
-        TileGenerators.permutations(["red", "black", "blue", "white"]),
+        TileGenerators.permutations([
+            "#00c0ef",
+            "#dd4b39",
+            "#f39c12",
+            "#00a65a",
+        ]),
+        TileGenerators.repeat(3),
     ],
 });
 
-export const defaultGameList = ["square", "triangle"];
+lookup.set("hexagons", {
+    atlas: HexagonsAtlas,
+    initialTile: [
+        "#00c0ef",
+        "#dd4b39",
+        "#f39c12",
+        "#00a65a",
+        "#bf7e16",
+        "#3c8dbc",
+    ],
+    tilesShownOnStack: 3,
+    tileGenerator: [
+        // TileGenerators.permutations(["red", "black", "blue", "white"]),
+        TileGenerators.permutations([
+            "#00c0ef",
+            "#dd4b39",
+            "#f39c12",
+            "#00a65a",
+            "#bf7e16",
+            "#3c8dbc",
+        ]),
+        TileGenerators.randomSubset(70),
+    ],
+});
+
+lookup.set("rhombus", {
+    atlas: RhombusAtlas,
+    initialTile: ["#00c0ef", "#dd4b39", "#f39c12", "#00a65a"],
+    tilesShownOnStack: 3,
+    tileGenerator: [
+        // TileGenerators.permutations(["red", "black", "blue", "white"]),
+        TileGenerators.permutations([
+            "#00c0ef",
+            "#dd4b39",
+            "#f39c12",
+            "#00a65a",
+        ]),
+    ],
+});
+
+export const defaultGameList = ["square", "triangle", "rhombus", "hexagons"];
 
 // ["triangle", "square", "isometric", "hex"]

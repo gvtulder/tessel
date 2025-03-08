6c445bd7d881c4e7ae4c03adedcfa98dffa1d638
Gijs van Tulder <gijs@vantulder.net>
2025-03-08 18:19:25 +0100

    Add ArraySet class.

---
+++ src/geom/ArraySet.test.ts
@@ -0,0 +1,39 @@
+import { describe, expect, test } from "@jest/globals";
+import { ArraySet } from "./ArraySet";
+
+describe("ArraySet", () => {
+    test("can add items", () => {
+        const set = new ArraySet();
+        set.add([1, 2, 3]);
+        set.add([4, 3, 2]);
+        set.add([1, 2, 3]);
+
+        expect([...set.values()]).toStrictEqual([
+            [1, 2, 3],
+            [4, 3, 2],
+        ]);
+
+        expect(set.size).toBe(2);
+
+        expect(set.has([1, 2, 3])).toBe(true);
+
+        expect(set.has([4, 5, 6])).toBe(false);
+        expect(set.has([4, 5])).toBe(false);
+        expect(set.has([4, 5, 6, 7])).toBe(false);
+
+        expect(set.has([])).toBe(false);
+        set.add([]);
+        expect(set.has([])).toBe(true);
+
+        set.delete([]);
+        set.delete([1, 2, 3]);
+        set.delete([1, 2, 3]);
+        expect(set.has([])).toBe(false);
+        expect(set.has([1, 2, 3])).toBe(false);
+    });
+
+    test("can be initialized with something", () => {
+        const set = new ArraySet([1, 2, 3]);
+        expect(set.has([1, 2, 3])).toBe(true);
+    });
+});
---
+++ src/geom/ArraySet.ts
@@ -0,0 +1,55 @@
+/**
+ * Set-like object that looks inside arrays for equality.
+ */
+export class ArraySet<T> {
+    private elements: T[][];
+
+    constructor(...values: T[][]) {
+        this.elements = [];
+        for (const v of values) {
+            this.add(v);
+        }
+    }
+
+    get size(): number {
+        return this.elements.length;
+    }
+
+    add(value: T[]): this {
+        if (this.indexOf(value) === -1) {
+            this.elements.push(value);
+        }
+        return this;
+    }
+
+    has(value: readonly T[]): boolean {
+        return this.indexOf(value) !== -1;
+    }
+
+    delete(value: readonly T[]): this {
+        const index = this.indexOf(value);
+        if (index !== -1) {
+            this.elements.splice(index, 1);
+        }
+        return this;
+    }
+
+    values(): ArrayIterator<T[]> {
+        return this.elements.values();
+    }
+
+    private indexOf(value: readonly T[]): number {
+        const elements = this.elements;
+        for (let idx = 0; idx < elements.length; idx++) {
+            const el = elements[idx];
+            if (value.length === el.length) {
+                let match = true;
+                for (let i = 0; match && i < el.length; i++) {
+                    if (value[i] != el[i]) match = false;
+                }
+                if (match) return idx;
+            }
+        }
+        return -1;
+    }
+}

a92e07e9d2ac3533886bbf253ddcb194523fcffe
Gijs van Tulder <gijs@vantulder.net>
2025-03-08 23:29:44 +0100

    Use square for rules option.

--- src/ui/setup/RulesOption.ts
+++ src/ui/setup/RulesOption.ts
@@ -20,16 +20,43 @@ export class RulesOption extends SettingRowOption {
 
         // gradient
         const defs = SVG("defs", null, svg);
-        const radialGradient = SVG("radialGradient", null, defs);
-        radialGradient.setAttribute("id", "gradient");
-        radialGradient.setAttribute("cx", "0.5");
-        radialGradient.setAttribute("cy", "0.5");
-        radialGradient.setAttribute("r", "0.4");
-        const start = SVG("stop", null, radialGradient);
-        start.setAttribute("offset", "70%");
-        start.setAttribute("stop-color", "white");
-        const stop = SVG("stop", null, radialGradient);
-        stop.setAttribute("offset", "100%");
+        let linearGradient;
+        linearGradient = SVG("linearGradient", null, defs);
+        linearGradient.setAttribute("id", "gradientH");
+        linearGradient.setAttribute("x1", "0");
+        linearGradient.setAttribute("x2", "0");
+        linearGradient.setAttribute("y1", "0");
+        linearGradient.setAttribute("y2", "1");
+        let stop;
+        stop = SVG("stop", null, linearGradient);
+        stop.setAttribute("offset", "4%");
+        stop.setAttribute("stop-color", "black");
+        stop = SVG("stop", null, linearGradient);
+        stop.setAttribute("offset", "15%");
+        stop.setAttribute("stop-opacity", "0");
+        stop = SVG("stop", null, linearGradient);
+        stop.setAttribute("offset", "85%");
+        stop.setAttribute("stop-opacity", "0");
+        stop = SVG("stop", null, linearGradient);
+        stop.setAttribute("offset", "96%");
+        stop.setAttribute("stop-color", "black");
+        linearGradient = SVG("linearGradient", null, defs);
+        linearGradient.setAttribute("id", "gradientV");
+        linearGradient.setAttribute("x1", "0");
+        linearGradient.setAttribute("x2", "1");
+        linearGradient.setAttribute("y1", "0");
+        linearGradient.setAttribute("y2", "0");
+        stop = SVG("stop", null, linearGradient);
+        stop.setAttribute("offset", "4%");
+        stop.setAttribute("stop-color", "black");
+        stop = SVG("stop", null, linearGradient);
+        stop.setAttribute("offset", "15%");
+        stop.setAttribute("stop-color", "white");
+        stop = SVG("stop", null, linearGradient);
+        stop.setAttribute("offset", "85%");
+        stop.setAttribute("stop-color", "white");
+        stop = SVG("stop", null, linearGradient);
+        stop.setAttribute("offset", "96%");
         stop.setAttribute("stop-color", "black");
 
         const g = SVG("g", null, svg);
@@ -45,17 +72,24 @@ export class RulesOption extends SettingRowOption {
         // mask
         const mask = SVG("mask", null, svg);
         mask.setAttribute("id", "mask");
-        const maskSquare = SVG("rect", null, mask);
+        let maskSquare;
+        maskSquare = SVG("rect", null, mask);
         maskSquare.setAttribute("x", "0");
         maskSquare.setAttribute("y", "0");
         maskSquare.setAttribute("width", "1");
         maskSquare.setAttribute("height", "1");
-        maskSquare.setAttribute("fill", "url(#gradient)");
+        maskSquare.setAttribute("fill", "url(#gradientV)");
+        maskSquare = SVG("rect", null, mask);
+        maskSquare.setAttribute("x", "0");
+        maskSquare.setAttribute("y", "0");
+        maskSquare.setAttribute("width", "1");
+        maskSquare.setAttribute("height", "1");
+        maskSquare.setAttribute("fill", "url(#gradientH)");
         g.setAttribute("mask", "url(#mask)");
 
         // checkmark
         const checkG = SVG("g", null, svg);
-        checkG.setAttribute("transform", "translate(0.70 0.7) scale(0.15)");
+        checkG.setAttribute("transform", "translate(0.63 0.63) scale(0.15)");
 
         // checkmark circle
         const circleBG = SVG("circle", null, mask);
@@ -63,7 +97,7 @@ export class RulesOption extends SettingRowOption {
         circleBG.setAttribute("cy", "0.45");
         circleBG.setAttribute("r", "1.2");
         circleBG.setAttribute("fill", "black");
-        circleBG.setAttribute("transform", "translate(0.70 0.7) scale(0.15)");
+        circleBG.setAttribute("transform", "translate(0.63 0.63) scale(0.15)");
 
         // checkmark circle
         const circle = SVG("circle", null, checkG);

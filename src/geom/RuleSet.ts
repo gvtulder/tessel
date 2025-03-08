582b8cc8915272c7d490de30f24c410b12710f46
Gijs van Tulder <gijs@vantulder.net>
2025-03-08 22:15:39 +0100

    Implement DifferentEdgeColorsRuleSet.

--- src/geom/RuleSet.ts
+++ src/geom/RuleSet.ts
@@ -58,7 +58,19 @@ export class MatchEdgeColorsRuleSet implements RuleSet {
  */
 export class DifferentEdgeColorsRuleSet implements RuleSet {
     checkColors(tile: Tile, colors: TileColors, offset?: number): boolean {
-        throw new Error("not implemented");
+        const edges = tile.edges;
+        const n = edges.length;
+        if (colors.length != n) return false;
+        if (offset === undefined) offset = 0;
+        for (let i = 0; i < n; i++) {
+            const edge = edges[(i + n - offset) % n];
+            if (!edge) continue;
+            if (edge.tileA && edge.tileA.colors![edge.edgeIdxA!] == colors[i])
+                return false;
+            if (edge.tileB && edge.tileB.colors![edge.edgeIdxB!] == colors[i])
+                return false;
+        }
+        return true;
     }
 
     computeColorConstraints(tileSegment: TileSegment): {

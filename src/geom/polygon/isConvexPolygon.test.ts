aa6f012c3a9d7fefb9c5edb6252fb2d0c733d63c
Gijs van Tulder <gijs@vantulder.net>
2025-03-11 15:30:30 +0100

    Add isConvexPolygon.

---
+++ src/geom/polygon/isConvexPolygon.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, test } from "@jest/globals";
+import { P, Point } from "../math";
+import { isConvexPolygon } from "./isConvexPolygon";
+
+describe("isConvexPolygon", () => {
+    test.each([
+        [P([0, 0], [1, 0], [1, 1]), true],
+        [P([0, 0], [1, 1], [1, 0]), true],
+        [P([0, 0], [1, 0], [1, 1], [0, 1]), true],
+        [P([0, 0], [1, 0], [0.5, 0.5], [1, 1], [0, 1]), false],
+        [[], false],
+    ])("can test polygon", (points: readonly Point[], expected: boolean) => {
+        expect(isConvexPolygon(points)).toBe(expected);
+    });
+});
---
+++ src/geom/polygon/isConvexPolygon.ts
@@ -0,0 +1,117 @@
+import { Point } from "../math";
+
+const EPS = 1e-6;
+
+/**
+ * Checks if the polygon is convex.
+ */
+export function isConvexPolygon(vertices: readonly Point[]): boolean {
+    // https://math.stackexchange.com/a/1745427
+
+    const n = vertices.length;
+    if (n < 3) return false;
+
+    // First nonzero orientation (positive or negative)
+    let wSign = 0;
+    let xSign = 0;
+    // Sign of first nonzero edge vector x
+    let xFirstSign = 0;
+    // Number of sign changes in x
+    let xFlips = 0;
+
+    let ySign = 0;
+    // Sign of first nonzero edge vector y
+    let yFirstSign = 0;
+    // Number of sign changes in y
+    let yFlips = 0;
+
+    // Second-to-last vertex
+    let curr = vertices[n - 2];
+    // Last vertex
+    let next = vertices[n - 1];
+
+    for (const v of vertices) {
+        // Previous vertex
+        const prev = curr;
+        // Current vertex
+        curr = next;
+        // Next vertex
+        next = v;
+
+        // Previous edge vector ("before"):
+        const bx = curr.x - prev.x;
+        const by = curr.y - prev.y;
+
+        // Next edge vector ("after"):
+        const ax = next.x - curr.x;
+        const ay = next.y - curr.y;
+
+        // Calculate sign flips using the next edge vector ("after"),
+        // recording the first sign.
+        if (ax > EPS) {
+            if (xSign == 0) {
+                xFirstSign = +1;
+            } else if (xSign < 0) {
+                xFlips++;
+            }
+            xSign = +1;
+        } else if (ax < -EPS) {
+            if (xSign == 0) {
+                xFirstSign = -1;
+            } else if (xSign > 0) {
+                xFlips++;
+            }
+            xSign = -1;
+        }
+
+        if (xFlips > 2) {
+            return false;
+        }
+
+        if (ay > EPS) {
+            if (ySign == 0) {
+                yFirstSign = +1;
+            } else if (ySign < 0) {
+                yFlips++;
+            }
+            ySign = +1;
+        } else if (ay < EPS) {
+            if (ySign == 0) {
+                yFirstSign = -1;
+            } else if (ySign > 0) {
+                yFlips++;
+            }
+            ySign = -1;
+        }
+
+        if (yFlips > 2) {
+            return false;
+        }
+
+        // Find out the orientation of this pair of edges,
+        // and ensure it does not differ from previous ones.
+        const w = bx * ay - ax * by;
+        if (wSign == 0 && Math.abs(w) > EPS) {
+            wSign = w;
+        } else if (wSign > 0 && w < -EPS) {
+            return false;
+        } else if (wSign < 0 && w > EPS) {
+            return false;
+        }
+    }
+
+    // Final/wraparound sign flips:
+    if (xSign != 0 && xFirstSign != 0 && xSign != xFirstSign) {
+        xFlips++;
+    }
+    if (ySign != 0 && yFirstSign != 0 && ySign != yFirstSign) {
+        yFlips++;
+    }
+
+    // Concave polygons have two sign flips along each axis.
+    if (xFlips != 2 || yFlips != 2) {
+        return false;
+    }
+
+    return true;
+}

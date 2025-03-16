a21cbd40ea68a10e81e1a0a49c2a5fbf8c8a5b85
Gijs van Tulder <gijs@vantulder.net>
2025-03-16 16:31:16 +0100

    Update GridBuilder and GridColoring tests.

--- src/grid/GridColoring.test.ts
+++ src/grid/GridColoring.test.ts
@@ -3,7 +3,7 @@ import { CentricGridBuilder } from "./GridBuilder";
 import { SquaresAtlas } from "./Atlas";
 import { GridColoring } from "./GridColoring";
 import { seedPRNG } from "../geom/RandomSampler";
-import { ColorPattern } from "./Shape";
+import { ColorPattern, Shape } from "./Shape";
 import { DifferentEdgeColorsRuleSet } from "./RuleSet";
 
 describe("GridColoring", () => {
@@ -29,52 +29,42 @@ describe("GridColoring", () => {
 
     test.each([
         [
-            [
-                {
-                    numColors: 4,
-                    segmentColors: [[0, 1, 2, 3]],
-                },
-            ],
+            {
+                numColors: 4,
+                segmentColors: [[0, 1, 2, 3]],
+            },
             400,
             4,
         ],
         [
-            [
-                {
-                    numColors: 2,
-                    segmentColors: [
-                        [0, 0, 1, 1],
-                        [0, 1, 1, 0],
-                    ],
-                },
-            ],
+            {
+                numColors: 2,
+                segmentColors: [
+                    [0, 0, 1, 1],
+                    [0, 1, 1, 0],
+                ],
+            },
             200,
             2,
         ],
         [
-            [
-                {
-                    numColors: 1,
-                    segmentColors: [[0, 0, 0, 0]],
-                },
-            ],
+            {
+                numColors: 1,
+                segmentColors: [[0, 0, 0, 0]],
+            },
             100,
             1,
         ],
     ])(
         "applies different-edge rules",
-        (
-            patterns: ColorPattern[],
-            numGroups: number,
-            numGroupsInTile: number,
-        ) => {
+        (pattern: ColorPattern, numGroups: number, numGroupsInTile: number) => {
             const grid = buildGrid();
             grid.rules = new DifferentEdgeColorsRuleSet();
             const coloring = new GridColoring(grid);
             expect(coloring.groups.size).toBe(400);
             const prng = seedPRNG(12345);
-            const colorPattern = new Map();
-            colorPattern.set(grid.atlas.shapes[0], patterns);
+            const colorPattern = new Map<Shape, ColorPattern>();
+            colorPattern.set(grid.atlas.shapes[0], pattern);
             coloring.applyColorPattern(colorPattern, false, prng);
             expect(coloring.groups.size).toBe(numGroups);
             for (const tile of grid.tiles) {
@@ -88,71 +78,59 @@ describe("GridColoring", () => {
 
     test.each([
         [
-            [
-                {
-                    numColors: 4,
-                    segmentColors: [[0, 1, 2, 3]],
-                },
-            ],
-            227,
+            {
+                numColors: 4,
+                segmentColors: [[0, 1, 2, 3]],
+            },
+            225,
         ],
         [
-            [
-                {
-                    numColors: 2,
-                    segmentColors: [
-                        [0, 0, 1, 1],
-                        [0, 1, 1, 0],
-                    ],
-                },
-            ],
-            31,
+            {
+                numColors: 2,
+                segmentColors: [
+                    [0, 0, 1, 1],
+                    [0, 1, 1, 0],
+                ],
+            },
+            27,
         ],
         [
-            [
-                {
-                    numColors: 1,
-                    segmentColors: [[0, 0, 0, 0]],
-                },
-            ],
+            {
+                numColors: 1,
+                segmentColors: [[0, 0, 0, 0]],
+            },
             1,
         ],
-    ])(
-        "applies color patterns",
-        (patterns: ColorPattern[], numGroups: number) => {
-            const grid = buildGrid();
-            const coloring = new GridColoring(grid);
-            expect(coloring.groups.size).toBe(227);
-            const prng = seedPRNG(12345);
-            const colorPattern = new Map();
-            colorPattern.set(grid.atlas.shapes[0], patterns);
-            coloring.applyColorPattern(colorPattern, false, prng);
-            expect(coloring.groups.size).toBe(numGroups);
-        },
-    );
-
-    test("applies color patterns with uniqueTileColors", () => {
-        const patterns = [
-            {
-                numColors: 33,
-                segmentColors: [[0, 0, 1, 2]],
-            },
-        ];
-
+    ])("applies color patterns", (pattern: ColorPattern, numGroups: number) => {
         const grid = buildGrid();
         const coloring = new GridColoring(grid);
-        expect(coloring.groups.size).toBe(227);
+        expect(coloring.groups.size).toBe(225);
         const prng = seedPRNG(12345);
         const colorPattern = new Map();
-        colorPattern.set(grid.atlas.shapes[0], patterns);
+        colorPattern.set(grid.atlas.shapes[0], pattern);
+        coloring.applyColorPattern(colorPattern, false, prng);
+        expect(coloring.groups.size).toBe(numGroups);
+    });
+
+    test("applies color patterns with uniqueTileColors", () => {
+        const pattern = {
+            numColors: 33,
+            segmentColors: [[0, 0, 1, 2]],
+        };
+        const grid = buildGrid();
+        const coloring = new GridColoring(grid);
+        expect(coloring.groups.size).toBe(225);
+        const prng = seedPRNG(12345);
+        const colorPattern = new Map();
+        colorPattern.set(grid.atlas.shapes[0], pattern);
         coloring.applyColorPattern(colorPattern, true, prng);
-        expect(coloring.groups.size).toBe(227);
+        expect(coloring.groups.size).toBe(225);
     });
 
     test("assigns colors", () => {
         const grid = buildGrid();
         const coloring = new GridColoring(grid);
-        expect(coloring.groups.size).toBe(227);
+        expect(coloring.groups.size).toBe(225);
         coloring.assignColors(["red", "green", "blue"]);
     });
 

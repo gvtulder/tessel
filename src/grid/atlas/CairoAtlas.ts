54a95107398c0b18e0dcd66fff1a16ad86af49b9
Gijs van Tulder <gijs@vantulder.net>
2025-03-18 18:39:01 +0100

    Set preferred setupSegments angle for Cairo5.

--- src/grid/atlas/CairoAtlas.ts
+++ src/grid/atlas/CairoAtlas.ts
@@ -16,6 +16,7 @@ export const CairoAtlas = Atlas.fromDefinition({
             ],
             preferredAngles: {
                 setupAtlas: 180,
+                setupSegments: 180,
             },
         },
     },

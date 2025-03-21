accd589b408e3f3de08b1a44bd41615ca2554726
Gijs van Tulder <gijs@vantulder.net>
2025-03-22 00:15:11 +0100

    Fix Jest tests with SVG import.

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,8 @@ export default {
     //  "^.+.tsx?$": ["ts-jest",{}],
     "^.+.tsx?$": ["@swc/jest"],
   },
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "identity-obj-proxy",
+  },
 };

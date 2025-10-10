32c75f0618edfae8dc1aba1628be6893a94bee28
Gijs van Tulder <gijs@vantulder.net>
2025-10-10 12:25:31 +0200

    Add Android version script.

---
+++ scripts/update-android-version.py
@@ -0,0 +1,24 @@
+import argparse
+import os.path
+import re
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--version", required=True)
+parser.add_argument("--build-gradle-path",
+    default=os.path.join(os.path.dirname(__file__), "../android/app/build.gradle"))
+args = parser.parse_args()
+
+if not re.match(r"^[0-9]+\.[0-9]+\.[0-9]+$", args.version):
+    raise Exception("invalid version")
+
+split_version = args.version.split(".")
+version_code = int("%d%02d%02d" % tuple(int(i) for i in split_version))
+
+with open(args.build_gradle_path, "r") as f:
+    txt = f.read()
+
+txt = re.sub(r'^(\s+versionCode\s+)([0-9]+)', fr'\g<1>{version_code}', txt, flags=re.M)
+txt = re.sub(r'^(\s+versionName\s+")([0-9.]+)(")', fr'\g<1>{args.version}\g<3>', txt, flags=re.M)
+
+with open(args.build_gradle_path, "w") as f:
+    f.write(txt)

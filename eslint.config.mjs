e921e8c57111e249dee44cf189c2381188dad355
Gijs van Tulder <gijs@vantulder.net>
2025-10-10 13:13:11 +0200

    Add eslint-plugin-license-header.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import licenseHeader from "eslint-plugin-license-header";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config([
@@ -12,8 +13,16 @@ export default tseslint.config([
       tseslint.configs.recommended,
       eslintConfigPrettier,
     ],
+  },
+  {
+    files: ["src/**/*.ts", "webpack.config.js", "eslint.config.mjs"],
+    ignores: ["src/lib/*.ts"],
+    plugins: {
+      "license-header": licenseHeader,
+    },
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
+      "license-header/header": ["error", "./license-header.ts"],
     },
   },
 ]);
---
+++ license-header.ts
@@ -0,0 +1,4 @@
+/**
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-FileCopyrightText: Copyright (C) 2025 Gijs van Tulder
+ */

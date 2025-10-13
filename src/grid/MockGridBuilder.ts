e66778e96127a94798ec786297bdce5232c3c55e
Gijs van Tulder <gijs@vantulder.net>
2025-10-14 01:36:53 +0200

    Document MockGridBuilder.

--- src/grid/MockGridBuilder.ts
+++ src/grid/MockGridBuilder.ts
@@ -7,16 +7,37 @@ import { Grid } from "./Grid";
 import { Tile } from "./Tile";
 import { SquaresAtlas } from "./atlas/SquaresAtlas";
 
+/**
+ * Shorthand for null in the grid defintions.
+ */
 export const _ = null;
+
+/**
+ * Defines a mock grid in a 2D array.
+ *
+ * For row r and column c:
+ * - A number def[r][c] = t indicates a tile inserted at step t.
+ * - def[r][c] = null (or _) indicates no tile at that position.
+ */
 export type MockGridDefinition = (number | null)[][];
 
+/**
+ * Simulates the construction of a tile grid given the definition.
+ */
 export class MockGridBuilder {
+    /**
+     * Returns a new MockGridBuilder that implements the given pattern.
+     *
+     * @param def the grid definition
+     * @returns a new MockGridBuilder
+     */
     static parseGridDefinition(def: MockGridDefinition): MockGridBuilder {
         const shape = SquaresAtlas.shapes[0];
         const grid = new Grid(SquaresAtlas);
         const tiles = new Map<string, Tile>();
         const queue: [number, number][] = [];
 
+        // convert row, column coordinate to string
         const C = (i: number, j: number) => `${i.toFixed(2)} ${j.toFixed(2)}`;
 
         // find a starting tile
@@ -45,11 +66,14 @@ export class MockGridBuilder {
                 { r: r, c: c - 1, edgeIndex: 1, otherEdgeIndex: 3 },
                 { r: r, c: c + 1, edgeIndex: 3, otherEdgeIndex: 1 },
             ]) {
+                // check that we are still inside the definition
                 if (dir.r < 0 || dir.r >= def.length) continue;
                 if (dir.c < 0 || dir.c >= def[dir.r].length) continue;
                 if (def[dir.r][dir.c] === null) continue;
+                // have we already processed this tile?
                 if (tiles.has(C(dir.r, dir.c))) continue;
-                // create new tile
+
+                // create new tile and add to the queue
                 const poly = shape.constructPolygonEdge(
                     tile.polygon.outsideEdges[dir.edgeIndex],
                     dir.otherEdgeIndex,
@@ -63,28 +87,44 @@ export class MockGridBuilder {
         return new MockGridBuilder(grid, tileSequence);
     }
 
+    /**
+     * The original grid with all tiles already inserted.
+     */
     private originGrid: Grid;
+    /**
+     * The tiles in order of insertion.
+     */
     tileSequence: Tile[][];
+    /**
+     * The mock grid.
+     */
     grid: Grid;
 
+    /**
+     * Constructs a new MockGridBuilder.
+     * @param originGrid the grid containing all tiles
+     * @param tileSequence the tiles to be inserted in each step
+     */
     constructor(originGrid: Grid, tileSequence: Tile[][]) {
         this.originGrid = originGrid;
         this.tileSequence = tileSequence;
         this.grid = new Grid(originGrid.atlas);
     }
 
+    /**
+     * Inserts the tiles for the given step into the mock grid.
+     * @param step the step in the sequence
+     * @returns the new tiles inserted in the grid
+     */
     applyStep(step: number): Tile[] {
-        const tiles: Tile[] = [];
-        if (this.tileSequence[step]) {
-            for (const t of this.tileSequence[step]) {
-                tiles.push(
-                    this.grid.addTile(t.shape, t.polygon, t.polygon.segment()),
-                );
-            }
-        }
-        return tiles;
+        return (this.tileSequence[step] || []).map((tile) =>
+            this.grid.addTile(tile.shape, tile.polygon, tile.polygon.segment()),
+        );
     }
 
+    /**
+     * The number of steps in the mock sequence.
+     */
     get numberOfSteps(): number {
         return this.tileSequence.length;
     }

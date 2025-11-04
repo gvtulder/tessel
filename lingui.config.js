19846913d41d1759c3eb83661977e019eacd2bf7
Jeng <gheescoo@outlook.com>
2025-11-04 16:04:09 +0800

    zh-Hant translation

--- lingui.config.js
+++ lingui.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "@lingui/cli";
 
 export default defineConfig({
   sourceLocale: "en",
-  locales: ["en", "nl"],
+  locales: ["en", "nl", "zh-Hant"],
   catalogs: [
     {
       path: "<rootDir>/i18n/{locale}",

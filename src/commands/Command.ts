4f0a1c30a54856edf5fa5c6b32112af07e03fedf
Gijs van Tulder <gijs@vantulder.net>
2025-12-23 17:26:08 +0100

    Add documentation for undo/redo classes.

--- src/commands/Command.ts
+++ src/commands/Command.ts
@@ -6,8 +6,25 @@
 import { PRNG } from "../geom/RandomSampler";
 import { Shape } from "../grid/Shape";
 
+/**
+ * A command for the undo/redo history encapsulating a command
+ * and its parameters.
+ */
 export abstract class Command {
+    /**
+     * Executes the command.
+     * @param prng an optional random number source
+     */
     abstract execute(prng?: PRNG): void;
+
+    /**
+     * Reverts the command.
+     */
     abstract undo(): void;
+
+    /**
+     * Serializes the command state.
+     * @param shapeMap a mapping from numbers to shape objects
+     */
     abstract save(shapeMap: readonly Shape[]): unknown;
 }
--- src/commands/CommandHistory.ts
+++ src/commands/CommandHistory.ts
@@ -5,11 +5,29 @@
 
 import { Command } from "./Command";
 
+/**
+ * Models the undo/redo history.
+ */
 export class CommandHistory extends EventTarget {
+    /**
+     * The stack of commands that can be undone.
+     */
     history: Command[];
+
+    /**
+     * The stack of commands that can be redone.
+     */
     future: Command[];
+
+    /**
+     * Callback function to be called when the history state changes.
+     */
     callback?: () => void;
 
+    /**
+     * Constructs a new command history.
+     * @param callback a function to be called when the history state changes (undo, redo, a new command)
+     */
     constructor(callback?: () => void) {
         super();
         this.history = [];
@@ -17,12 +35,21 @@ export class CommandHistory extends EventTarget {
         this.callback = callback;
     }
 
+    /**
+     * Adds a command to the command history. This clears the redo list.
+     * @param command the latest command
+     */
     push(command: Command): void {
         this.history.push(command);
         this.future = [];
         if (this.callback) this.callback();
     }
 
+    /**
+     * Runs the undo function of the most recent command and moves this
+     * to the redo future list.
+     * @returns true if a command was undone, false if the undo history was empty
+     */
     undo(): boolean {
         const command = this.history.pop();
         if (!command) return false;
@@ -32,6 +59,11 @@ export class CommandHistory extends EventTarget {
         return true;
     }
 
+    /**
+     * Re-executes the most recently undone command and moves this to
+     * the undo history list.
+     * @returns true if a command was redone, false if the redo history was empty
+     */
     redo(): boolean {
         const command = this.future.pop();
         if (!command) return false;
@@ -41,16 +73,25 @@ export class CommandHistory extends EventTarget {
         return true;
     }
 
+    /**
+     * Clears the undo and redo stacks.
+     */
     reset(): void {
         this.history = [];
         this.future = [];
         if (this.callback) this.callback();
     }
 
+    /**
+     * Returns true if the undo stack is not empty.
+     */
     get canUndo(): boolean {
         return this.history.length > 0;
     }
 
+    /**
+     * Returns true if the redo stack is not empty.
+     */
     get canRedo(): boolean {
         return this.future.length > 0;
     }
